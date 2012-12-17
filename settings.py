2e839ec0afe21f1a07e64eb1ed16c996d0fdf205
Sri Prasanna <k.sriprasanna@gmail.com>
2012-12-17 20:24:26 +0530

    changes

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'mysite',                      # Or path to database file if using sqlite3.
-        'USER': 'snarkx',                      # Not used with sqlite3.
+        'USER': 'snark',                      # Not used with sqlite3.
         # 'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         # 'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
